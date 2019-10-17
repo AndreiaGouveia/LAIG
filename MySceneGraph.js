00b5af8b4b73e4cfdc0b300311f6e780f0a10c0c
filipasenra <filipasenra4@gmail.com>
2019-10-17 18:27:14 +0100

    Fixing bug in lights

--- MySceneGraph.js
+++ MySceneGraph.js
@@ -560,7 +560,7 @@ class MySceneGraph {
             if (!(aux != null && !isNaN(aux) && (aux == true || aux == false)))
                 this.onXMLMinorError("unable to parse value component of the 'enable light' field for ID = " + lightId + "; assuming 'value = 1'");
 
-            enableLight = aux || true;
+            enableLight = aux || false;
 
             //Add enabled boolean and type name to light info
             global.push(enableLight);
