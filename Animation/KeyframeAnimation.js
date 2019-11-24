004096063af45c0166e1e9eb49db754ab59975e8
filipasenra <filipasenra4@gmail.com>
2019-11-24 18:30:57 +0000

    Added Bridge

--- Animation/KeyframeAnimation.js
+++ Animation/KeyframeAnimation.js
@@ -27,50 +27,51 @@ class KeyframeAnimation extends Animation {
                 this.stage++;
             }
 
-            if (this.stage == this.keyFrames.length)
+            if (this.stage == this.keyFrames.length) {
                 this.endOfAnimation = true;
-            else {
-                var currentTransformation = this.keyFrames[this.stage];
-
-                var formerTransformation;
-
-                if (this.stage == 0)
-                    formerTransformation = new KeyframeModel(0, [0, 0, 0], [0, 0, 0], [1, 1, 1]);
-                else
-                    formerTransformation = this.keyFrames[this.stage - 1];
-
-                //Percentage Of Time from the former Transformation and the Current Transformation
-                var percentageTime = (this.timeElapsed - formerTransformation.endInstant) / (currentTransformation.endInstant - formerTransformation.endInstant);
-
-                //TRANSFORMATIONS
-                var translateX = this.lerp(formerTransformation.translation[0], currentTransformation.translation[0], percentageTime);
-                var translateY = this.lerp(formerTransformation.translation[1], currentTransformation.translation[1], percentageTime);
-                var translateZ = this.lerp(formerTransformation.translation[2], currentTransformation.translation[2], percentageTime);
-
-                mat4.translate(newMatrix, newMatrix, [translateX, translateY, translateZ]);
-
-                var rotateX = this.lerp(formerTransformation.rotation[0], currentTransformation.rotation[0], percentageTime) * DEGREE_TO_RAD;
-                mat4.rotate(newMatrix, newMatrix, rotateX, [1, 0, 0]); // rotation in x
-
-                var rotateY = this.lerp(formerTransformation.rotation[1], currentTransformation.rotation[1], percentageTime) * DEGREE_TO_RAD;
-                mat4.rotate(newMatrix, newMatrix, rotateY, [0, 1, 0]); // rotation in Y
-
-                var rotateZ = this.lerp(formerTransformation.rotation[2], currentTransformation.rotation[2], percentageTime) * DEGREE_TO_RAD;
-                mat4.rotate(newMatrix, newMatrix, rotateZ, [0, 0, 1]); // rotation in Z
-
-
-                var scaleX = this.lerp(formerTransformation.scale[0], currentTransformation.scale[0], percentageTime);
-                var scaleY = this.lerp(formerTransformation.scale[1], currentTransformation.scale[1], percentageTime);
-                var scaleZ = this.lerp(formerTransformation.scale[2], currentTransformation.scale[2], percentageTime);
-
-                mat4.scale(newMatrix, newMatrix, [scaleX, scaleY, scaleZ]);
-
-                //END TRANSFORMATION
-
-                this.animationMatrix = newMatrix;
+                this.stage--;
+                this.timeElapsed > this.keyFrames[this.stage].endInstant;
             }
+            var currentTransformation = this.keyFrames[this.stage];
 
+            var formerTransformation;
+
+            if (this.stage == 0)
+                formerTransformation = new KeyframeModel(0, [0, 0, 0], [0, 0, 0], [1, 1, 1]);
+            else
+                formerTransformation = this.keyFrames[this.stage - 1];
+
+            //Percentage Of Time from the former Transformation and the Current Transformation
+            var percentageTime = (this.timeElapsed - formerTransformation.endInstant) / (currentTransformation.endInstant - formerTransformation.endInstant);
+
+            //TRANSFORMATIONS
+            var translateX = this.lerp(formerTransformation.translation[0], currentTransformation.translation[0], percentageTime);
+            var translateY = this.lerp(formerTransformation.translation[1], currentTransformation.translation[1], percentageTime);
+            var translateZ = this.lerp(formerTransformation.translation[2], currentTransformation.translation[2], percentageTime);
+
+            mat4.translate(newMatrix, newMatrix, [translateX, translateY, translateZ]);
+
+            var rotateX = this.lerp(formerTransformation.rotation[0], currentTransformation.rotation[0], percentageTime) * DEGREE_TO_RAD;
+            mat4.rotate(newMatrix, newMatrix, rotateX, [1, 0, 0]); // rotation in x
+
+            var rotateY = this.lerp(formerTransformation.rotation[1], currentTransformation.rotation[1], percentageTime) * DEGREE_TO_RAD;
+            mat4.rotate(newMatrix, newMatrix, rotateY, [0, 1, 0]); // rotation in Y
+
+            var rotateZ = this.lerp(formerTransformation.rotation[2], currentTransformation.rotation[2], percentageTime) * DEGREE_TO_RAD;
+            mat4.rotate(newMatrix, newMatrix, rotateZ, [0, 0, 1]); // rotation in Z
+
+
+            var scaleX = this.lerp(formerTransformation.scale[0], currentTransformation.scale[0], percentageTime);
+            var scaleY = this.lerp(formerTransformation.scale[1], currentTransformation.scale[1], percentageTime);
+            var scaleZ = this.lerp(formerTransformation.scale[2], currentTransformation.scale[2], percentageTime);
+
+            mat4.scale(newMatrix, newMatrix, [scaleX, scaleY, scaleZ]);
+
+            //END TRANSFORMATION
+
+            this.animationMatrix = newMatrix;
         }
+
 
         this.scene.multMatrix(this.animationMatrix);
 
--- Primitives/MyPatch.js
+++ Primitives/MyPatch.js
@@ -1,34 +1,35 @@
-class MyPatch extends CGFobject{
-	constructor(scene,npartsU, npartsV, npointsU, npointsV, controlPoints
-        ) {
-		super(scene);
-            console.log(controlPoints);
-        this.controlvertexes=[];
-        
+class MyPatch extends CGFobject {
+    constructor(scene, nPartsU, nPartsV, nPointsU, nPointsV, controlPoints) {
+        super(scene);
+
+
+        var controlVertexes = [];
+
         //get our control Vertexes
 
-        for(var i=0 ; i<npointsU ;i++ ){
+        for (var i = 0; i < nPointsU; i++) {
+
             //Points U1....UN
             var pointsU = []; //create an empty set
-            for(var j=0 ; j<npointsV ; j++)
-            {
 
-                var point=[];
-                point.push(...controlPoints[i*npointsV+j]);
+            for (var j = 0; j < nPointsV; j++) {
+
+                var point = [];
+                point.push(...controlPoints[i * nPointsV + j]);
                 point.push(1); //because control points have w=1
                 pointsU.push(point);
             }
-            this.controlvertexes.push(pointsU);
-        }
-        console.log("final: ");
-        console.log(this.controlvertexes);
-        this.nurbsSurface = new CGFnurbsSurface(npointsU-1, npointsV-1,this.controlvertexes);
-		this.obj = new CGFnurbsObject(this.scene, npartsU, npartsV, this.nurbsSurface ); // must provide an object with the function getPoint(u, v) (CGFnurbsSurface has it)
-		
-	}
 
-	display(){
+            controlVertexes.push(pointsU);
+        }
+
+        this.nurbsSurface = new CGFnurbsSurface(nPointsU - 1, nPointsV - 1, controlVertexes);
+        this.obj = new CGFnurbsObject(this.scene, nPartsU, nPartsV, this.nurbsSurface); // must provide an object with the function getPoint(u, v) (CGFnurbsSurface has it)
+
+    }
+
+    display() {
         this.obj.display();
-	}
-	updateTexCoords(s, t) {}
+    }
+    updateTexCoords(s, t) {}
 }
