6480a10e72706321a52cf258934952baaaf11a2b
Andreia Gouveia <andiigouveia@hotmail.com>
2019-12-26 17:36:24 +0000

    ice cream done

--- LAIG_TP3_T5_G03/Primitives/MyIceCream.js
+++ LAIG_TP3_T5_G03/Primitives/MyIceCream.js
@@ -10,31 +10,45 @@ class MyIceCream extends CGFobject {
     constructor(scene) {
         super(scene);
 
-        this.cone = new Cylinder2(scene, 0, 2.5, 5, 10, 10);
+        this.cone = new Cylinder2(scene, 0, 2, 10, 10, 10);
         this.iceCream = new MySphere(scene, 0, 2.5, 10, 10);
         this.iceCream1 = new MySphere(scene, 0, 2, 10, 10);
         this.iceCream2 = new MySphere(scene, 0, 1.5, 10, 10);
+
+        
+        this.ice = new CGFappearance(scene);
+        this.ice.setAmbient(1, 1, 1, 1);
+        this.coneColor = new CGFappearance(scene);
+        this.coneColor.setAmbient(1, 1, 1, 1);
+
+        this.creamTexture = new CGFtexture(this.scene, "../scenes/images/cream.jpg");
+        this.coneTexture = new CGFtexture(this.scene, "../scenes/images/cone.jpg");
+
+        this.ice.setTexture(this.creamTexture);
+        this.coneColor.setTexture(this.coneTexture);
 
     }
 
     display() {
 
         this.scene.pushMatrix();
-        this.scene.translate(0,4,0);
+        this.ice.apply();
+        this.scene.translate(0,9,0);
         this.iceCream.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
-        this.scene.translate(0,6,0);
+        this.scene.translate(0,11,0);
         this.iceCream1.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
-        this.scene.translate(0,8,0);
+        this.scene.translate(0,13,0);
         this.iceCream2.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
+        this.coneColor.apply();
         this.scene.translate(0,0,1);
         this.scene.rotate(Math.PI,1.0,0.0,0.0)
         this.cone.display();
