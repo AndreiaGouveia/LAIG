5672caafa00dd1d74f817984c8eca6ce1e2adfad
Andreia Gouveia <andiigouveia@hotmail.com>
2019-12-28 02:34:43 +0000

    removed wrong project

--- LAIG_TP3_T5_G03/projecto_plog/T1/Menu.pl
+++ LAIG_TP3_T5_G03/projecto_plog/T1/Menu.pl
@@ -1,34 +1,54 @@
-menus:-
+menus:- % First Menu
+        repeat,
         displayMainMenu,
         nl,
-        read(Input),
+        read_line([H|T]),
+        length(T,0),
+        !,
+        Input is H-48,
         menuChoice(Input).
 
-menuChoice(1):-
+menuChoice(1):- % start game
         initialBoard(InitialBoard),
-        initGame(InitialBoard , 0).
+        length(InitialBoard, X),
+        X1 is X-2,
+        piecesBlack(BlackPieces,X1),
+        piecesWhite(WhitePieces,X1),
+        repeat,
+        displayGameMenu,
+        nl,
+        read_line([H|T]),
+        length(T,0),
+        !,
+        Input is H-48,
+        gameChoice(Input , InitialBoard , BlackPieces , WhitePieces).
 
-menuChoice(2):-
-        write('Thanks for playing!').
-
-menuChoice(_):-
-        write('Must choose between 1 or 2'),
-        read(Input),
-        menuChoice(Input).
-
-initGame(Board , 16):-
-        write('\n Game over!\n'),
+menuChoice(2):- % instructions
+        displayInstructions,
+        read_line(_),
         menus.
-       
-initGame(Board , Counter):-
-        showBoard(white,Board),
-        getPlay(Board , white),
-        Counter1 is Counter + 1,
-        showBoard(black,Board),
-        getPlay(Board , black),
-        Counter2 is Counter1 + 1,
-        initGame(Board , Counter2).
 
+menuChoice(3):- % credits
+        displayCredits,
+        read_line(_),
+        menus.
+
+menuChoice(4):- % exitgame
+        write('\nThanks for playing!\n').
+
+menuChoice(_):- % invalid input
+        write('Must choose between 1 or 4\n'),
+        menus.
+
+% ========== Init game option ==================
+gameChoice(1 , InitialBoard , BlackPieces , WhitePieces):- personVSperson(white,InitialBoard , 0, WhitePieces , BlackPieces) , menus. % person vs person
+gameChoice(2 , InitialBoard , BlackPieces , WhitePieces):- personVSpc(random,player,InitialBoard , 0 , WhitePieces, BlackPieces) , menus. % person vs pc (easy)
+gameChoice(3 , InitialBoard , BlackPieces , WhitePieces):-  personVSpc(smart,player,InitialBoard, 0 , WhitePieces , BlackPieces) , menus. % person vs pc (hard)
+gameChoice(4 , InitialBoard , BlackPieces , WhitePieces):-  pcVSpc(random,pc1,InitialBoard, 0 , WhitePieces , BlackPieces) , menus. % pc vs pc (easy)
+gameChoice(5 , InitialBoard , BlackPieces , WhitePieces):-  pcVSpc(smart,pc1,InitialBoard, 0 , WhitePieces , BlackPieces) , menus. % pc vs pc (hard)
+gameChoice(_ , _InitialBoard , _BlackPieces , _WhitePieces):- menuChoice(1). % invalid input
+
+% ============= Menus Display =====================
 displayMainMenu:-
         write('\n ----------------------------------------\n'),
         write('|                                        |\n'),
@@ -38,7 +58,62 @@ displayMainMenu:-
         write('|                                        |\n'),
         write('|         Options:                       |\n'),
         write('|                    1- Start Game       |\n'),
-        write('|                    2- Exit             |\n'),
+        write('|                    2- Instructions     |\n'),
+        write('|                    3- Credits          |\n'),
+        write('|                    4- Exit             |\n'),
         write('|                                        |\n'),
         write('|                                        |\n'),
         write(' ----------------------------------------\n').
+
+displayGameMenu:-
+        write('\n ------------------------------------------\n'),
+        write('|                                            |\n'),
+        write('|                   Quantik                  |\n'),
+        write('|                                            |\n'),
+        write('|                                            |\n'),
+        write('|    Options:                                |\n'),
+        write('|                                            |\n'),
+        write('|             1- Person vs Person            |\n'),
+        write('|             2- Computer vs Person (Easy)   |\n'),
+        write('|             3- Computer vs Person (Hard)   |\n'),
+        write('|             4- Computer vs Computer (Easy) |\n'),
+        write('|             5- Computer vs Computer (Hard) |\n'),
+        write('|                                            |\n'),
+        write(' --------------------------------------------\n').
+
+displayInstructions:-
+        write('\n ------------------------------------------\n'),
+        write('|                                            |\n'),
+        write('|                   Quantik                  |\n'),
+        write('|                                            |\n'),
+        write('| Instructions:                              |\n'),
+        write('|                                            |\n'),
+        write('| The goal is to be the first player to pose |\n'),
+        write('| the fourth different forms of a line, a    |\n'),
+        write('| column or a square zone. Each turn the     |\n'),
+        write('| players will put one of their pieces on    |\n'),
+        write('| the boardgame. It\'s forbidden to put a     |\n'),
+        write('| shape in a line, a column or an area on    |\n'),
+        write('| which this same form has already been      |\n'),
+        write('| posed by the opponent. We can only double  |\n'),
+        write('| a shape if we have played the previous one |\n'),
+        write('| ourself. The first player who places the   |\n'),
+        write('| fourth different form in a row, column or  |\n'),
+        write('| zone wins the game immediately, no matter  |\n'),
+        write('| who owns the other pieces of that winning  |\n'),
+        write('| move.                                      |\n'),
+        write('|                    (Press Enter to Escape) |\n'),
+        write(' --------------------------------------------\n\n').
+
+displayCredits:-
+        write('\n ------------------------------------------\n'),
+        write('|                                            |\n'),
+        write('|                   Quantik                  |\n'),
+        write('|                                            |\n'),
+        write('|                                            |\n'),
+        write('|   This game was made by:                   |\n'),
+        write('|                           Andreia Gomes    |\n'),
+        write('|                           Joao Araujo      |\n'),
+        write('|                                            |\n'),
+        write('|                    (Press Enter to Escape) |\n'),
+        write(' --------------------------------------------\n\n').
--- LAIG_TP3_T5_G03/projecto_plog/T1/Quantik.pl
+++ LAIG_TP3_T5_G03/projecto_plog/T1/Quantik.pl
@@ -1,5 +1,9 @@
 :- consult('Menu.pl').
 :- consult('display.pl').
 :- consult('Logica.pl').
+:- consult('Modos.pl').
+:- consult('bot.pl').
+:- use_module(library(lists)).
+:- use_module(library(random)).
 
-quantik:- menus.
+play:- menus.
